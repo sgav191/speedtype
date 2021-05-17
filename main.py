8e47b699896d5490114d23c46a5fe2110baf7268
sgav191 <swgavin0@gmail.com>
2021-05-17 20:40:11 +0100

    I added a message at the end of the game

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from os import system
 system("clear")
 phrases = ["Your mission, should you choose to accept it.","Desperate times, desperate measures.","Mission Accomplished!","Part of the journey is the end.","It's not about how much we lost. It's about how much we have left.","The hardest choices require the strongest wills.","We'll burn that bridge when we get to it."]
 print ("Welcome to speedtype!")
+name = input ("Please enter your name: ")
 time.sleep(1)
 instructionsquestion = input ("Would you like to see some instructions, y/n: ")
 if instructionsquestion == "y":
@@ -34,3 +35,5 @@ for currentphrase in phrases:
     else:
         print (f"Well done! It took you {timeroutput}secs to write the phrase. You wrote a total of {len(totalwords)} words, and there was {errors} mistakes.")
         input("Press enter when you're ready to move on to the next question...")
+
+print ("Yay! You've reached the end {name}!")
